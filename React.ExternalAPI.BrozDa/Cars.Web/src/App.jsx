821ca8d069aca722c8cb9e772aee4fc59d1af74c
Brozda <daniel.brozik@gmail.com>
2025-07-02 16:14:12 +0200

    feat: Adjust Load and error for mobiles

--- React.ExternalAPI.BrozDa/Cars.Web/src/App.jsx
+++ React.ExternalAPI.BrozDa/Cars.Web/src/App.jsx
@@ -1,15 +1,19 @@
 
 import Navbar from "./components/Navbar"
 import AppRoutes from "./appRoutes/AppRoutes"
+
+import Loading from "./components/Loading"
+import Error from "./components/Error"
 function App() {
    
   return (
-    <>
+    <Loading/>
+    /*<>
       <header>
         <Navbar />
       </header>
       <AppRoutes />
-    </>
+    </>*/
   )
 }
 
